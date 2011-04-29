cef66dc421d35fe5ee913844dd725a30cc2c2576
Andrew Parker <aparker42@gmail.com>
2011-04-29 11:04:10 +0100

    removing unneeded annotation for jaxb

--- src/main/java/org/bgprocess/keepup/Players.java
+++ src/main/java/org/bgprocess/keepup/Players.java
@@ -3,9 +3,6 @@ package org.bgprocess.keepup;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlRootElement;
-
-@XmlRootElement
 public class Players {
 	private static final Players instance = new Players();
 	
